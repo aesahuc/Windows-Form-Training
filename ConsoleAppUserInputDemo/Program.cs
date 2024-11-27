b361538a426eca7b2596da00640d6fa9615a3857
s4ndcl0ck3y3s <85315507+s4ndcl0ck3y3s@users.noreply.github.com>
2024-11-26 20:58:17 -0600

    Ampliando el ejemplo

--- ConsoleAppUserInputDemo/Program.cs
+++ ConsoleAppUserInputDemo/Program.cs
@@ -1,18 +1,36 @@
 ﻿// Declare variables
-string name = string.Empty;
-int age = 0;
+string firstName = string.Empty;
+string lastName = string.Empty;
+int age;
 int retirementAge = 65;
+decimal salary;
+char gender = char.MinValue;
+bool working = true;
 
 //Promt the user for input
-Console.WriteLine("Ingresa tu nombre por favor");
-name = Console.ReadLine();
-Console.WriteLine("Ingresa tu edad");
+Console.Write("Ingresa tu nombre, por favor: ");
+firstName = Console.ReadLine();
+
+Console.Write("Ingresa tu apellido, por favor: ");
+lastName = Console.ReadLine();
+
+Console.Write("Ingresa tu edad, por favor: ");
 age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ingresa tu genero, por favor.(M o F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Trabajando? (True o False): ");
+salary = Convert.ToBoolean(Console.ReadLine());
+
 
 //Process the data
 int workingAgesRemaining = retirementAge - age;
 
 //Output the results to the users
-Console.WriteLine($"Nombre: {name}");
+Console.WriteLine($"Nombre: {firstName}{lastName}");
 Console.WriteLine($"Edad: {age}");
+Console.WriteLine($"Tu salario es: {salary}");
+Console.WriteLine($"Tu genero es: {gender}");
+Console.WriteLine($"Estas empleado: {working}");
 Console.WriteLine($"Los años que falta para retirarse son {workingAgesRemaining}");
