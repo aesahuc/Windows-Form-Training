daf714af14b89dacf844384d420d48bdeb72a9b0
s4ndcl0ck3y3s <85315507+s4ndcl0ck3y3s@users.noreply.github.com>
2024-11-26 19:22:26 -0600

    Prueba de cambios para Git

---
+++ ConsoleApp1VariablesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Different data types
+
+/*
+    text - string
+    integers - int
+    decimals - double, float, decimal
+    logical - bool
+ */
+
+string name = "Esahu Colorado";
+
+Console.WriteLine(name);
+Console.WriteLine($"I am " + name); //String concatenation
+Console.WriteLine($"They call me {name}"); //String interpolation
+Console.WriteLine("I was given the name of {0}", name); //Formated String
+
+int age = 35;
+int retirementYearsLeft = 30;
+int retirementAge = age + retirementYearsLeft;
+Console.WriteLine("Mi age is " + age);
+Console.WriteLine("My retirement age is " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("I am retired? " + isRetired);
---
+++ ConsoleAppUserInputDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿// Declare variables
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+//Promt the user for input
+Console.WriteLine("Ingresa tu nombre por favor");
+name = Console.ReadLine();
+Console.WriteLine("Ingresa tu edad");
+age = Convert.ToInt32(Console.ReadLine());
+
+//Process the data
+int workingAgesRemaining = retirementAge - age;
+
+//Output the results to the users
+Console.WriteLine($"Nombre: {name}");
+Console.WriteLine($"Edad: {age}");
+Console.WriteLine($"Los años que falta para retirarse son {workingAgesRemaining}");
+
+Console.WriteLine($"Los años que falta para retirarse son {workingAgesRemaining}");
