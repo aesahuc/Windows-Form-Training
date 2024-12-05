74154aeaf8866bbeacdb348ee256a66df0e1b3ea
s4ndcl0ck3y3s <85315507+s4ndcl0ck3y3s@users.noreply.github.com>
2024-12-05 00:58:20 -0600

    Add Loops demo and update the calculator

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Simple for loop demo
+//Print Hello, World 10 times
+Console.WriteLine("Hello, World!");
+
+
+//Ask user how many times  they wish to print hello world and print accordingly
+//Console.WriteLine("How many times do you want to print the message?");
+//int times = Convert.ToInt32(Console.ReadLine());
+
+//for  (int i = 0; i < times; i++)
+//{
+//    Console.WriteLine($"Hello, World! - {i + 1} Times -");
+//}
+//Console.WriteLine("Loop completed!");
+
+
+Console.WriteLine("********** Simple for loop **********");
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Hello, world! - {i}");
+}
+
+Console.WriteLine("********** Simple for loop completed **********");
+
+
+//While loop 
+
+Console.WriteLine("********** Simple While loop **********");
+
+int count = 0;
+
+while(count < 10)
+{
+    Console.WriteLine($"Hello, world! - {count}");
+    count++;
+}
+
+
+Console.WriteLine("Se sumaran los numeros que se ingresen y se imprimira el total. Para salir ingrese" +
+    "numero negativo");
+int numero = 0;
+int suma = 0;
+while(numero >= 0)
+{
+    Console.Write("Ingresa el numero: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+    suma = suma + numero;
+    Console.WriteLine($"La suma de los numeros es de :{suma}");
+}
+
+Console.WriteLine("********** Simple While loop Completed **********");
+
+Console.WriteLine("********** Simple Do While loop **********");
+
+int num = 0;
+int sum = 0;
+do
+{
+    Console.Write("Ingresa el numero: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    sum = sum + num;
+    Console.WriteLine($"La suma de los numeros es de :{sum}");
+} while (num >= 0);
+
+Console.WriteLine("********** Simple Do While loop Completed **********");
+
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -2,50 +2,76 @@
 //Welcome Message
 Console.WriteLine("********** - Welcome to the sample calculator app - **********");
 
-//Prompt for user input
-Console.WriteLine("Please, enter the first number");
-decimal num1 = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Please, enter the second number");
-decimal num2 = Convert.ToDecimal(Console.ReadLine());
+int resp = 0;
 
 //Show calculator options
-
-Console.WriteLine("Please, select the number of the operation you want to execute.");
-
-Console.WriteLine("1.- Adition");
-Console.WriteLine("2.- Substraction");
-Console.WriteLine("3.- Multiply");
-Console.WriteLine("4.- Division");
-Console.WriteLine("5.- Modulus");
-
-int resp = Convert.ToInt32(Console.ReadLine());
-
-decimal resultado = 0;
-
-//Decide which operation is needed based on selected option
-
-switch (resp)
+do
 {
-    case 1:
-        resultado = num1 + num2; 
-        break;
-    case 2:
-        resultado = num1 - num2; 
-        break;
-    case 3:
-        resultado = num1 * num2;
-        break;
-    case 4:
-        resultado = num1 / num2;
-        break;
-    case 5:
-        resultado = num1 % num2;
-        break;
-    default:
-        resultado = 0;
-        Console.WriteLine("No se realizo ninguna operacion");
-    break;
-}
 
-Console.WriteLine($"El resultado de la operacion seleccionada es: {resultado}");
+    Console.WriteLine("Please, select the number of the operation you want to execute.");
+    Console.WriteLine("Para salir del programa, ingrese -1");
+    Console.WriteLine("1.- Adition");
+    Console.WriteLine("2.- Substraction");
+    Console.WriteLine("3.- Multiply");
+    Console.WriteLine("4.- Division");
+    Console.WriteLine("5.- Modulus");
+    Console.WriteLine("6.- Fibonacci sequence");
+
+    resp = Convert.ToInt32(Console.ReadLine());
+
+    if (resp >= 0)
+    {
+        //Prompt for user input
+        Console.WriteLine("Please, enter the first number");
+        decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Please, enter the second number");
+        decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+
+
+        decimal resultado = 0;
+
+        //Decide which operation is needed based on selected option
+
+        switch (resp)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                resultado = num1 / num2;
+                break;
+            case 5:
+                resultado = num1 % num2;
+                break;
+            case 6:
+                if (num2 <= num1)
+                {
+                    Console.WriteLine("El numero 2 debe de ser mayor al numero 1");
+                }
+                else
+                {
+                    for (decimal i = num1; i <= num2; i++)
+                    {
+                        resultado = resultado + i;
+                    }
+                }
+                break;
+            default:
+                resultado = 0;
+                Console.WriteLine("No se realizo ninguna operacion");
+                break;
+        }
+
+        Console.WriteLine($"El resultado de la operacion seleccionada es: {resultado}");
+    }
+} while (resp >= 0);
+
+Console.WriteLine("********** - Thank you for using the sample calculator app - **********");
