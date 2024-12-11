8284ebde19166ab16717b73b751bc09100e8d890
s4ndcl0ck3y3s <85315507+s4ndcl0ck3y3s@users.noreply.github.com>
2024-12-11 15:21:19 -0600

    Added exception handling and modify the calculator app.

---
+++ ConsoleApp.ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Write a program that take a user's age as input and prints it to the screen.
+//Display an error message if an invalid input is received.
+
+
+
+try
+{
+    Console.Write("Ingresa tu edad: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Tu tienes {age} años.");
+}
+catch
+{
+    Console.WriteLine("El valor en la edad es incorrecto, por favor, intenta de nuevo.");
+}
+finally
+{
+    Console.WriteLine("********** Gracias por usar este programa **********");
+}
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -3,75 +3,91 @@
 Console.WriteLine("********** - Welcome to the sample calculator app - **********");
 
 int resp = 0;
+decimal num1 = 0;
+decimal num2 = 0;
 
 //Show calculator options
 do
 {
-
-    Console.WriteLine("Please, select the number of the operation you want to execute.");
-    Console.WriteLine("Para salir del programa, ingrese -1");
-    Console.WriteLine("1.- Adition");
-    Console.WriteLine("2.- Substraction");
-    Console.WriteLine("3.- Multiply");
-    Console.WriteLine("4.- Division");
-    Console.WriteLine("5.- Modulus");
-    Console.WriteLine("6.- Fibonacci sequence");
-
-    resp = Convert.ToInt32(Console.ReadLine());
-
-    if (resp >= 0)
+    try
     {
-        //Prompt for user input
-        Console.WriteLine("Please, enter the first number");
-        decimal num1 = Convert.ToDecimal(Console.ReadLine());
-
-        Console.WriteLine("Please, enter the second number");
-        decimal num2 = Convert.ToDecimal(Console.ReadLine());
-
-
-
-        decimal resultado = 0;
-
-        //Decide which operation is needed based on selected option
-
-        switch (resp)
+        Console.WriteLine("Please, select the number of the operation you want to execute.");
+        Console.WriteLine("Para salir del programa, ingrese -1");
+        Console.WriteLine("1.- Adition");
+        Console.WriteLine("2.- Substraction");
+        Console.WriteLine("3.- Multiply");
+        Console.WriteLine("4.- Division");
+        Console.WriteLine("5.- Modulus");
+        Console.WriteLine("6.- Fibonacci sequence");
+        resp = Convert.ToInt32(Console.ReadLine());
+        if (resp >= 0)
         {
-            case 1:
-                resultado = num1 + num2;
-                break;
-            case 2:
-                resultado = num1 - num2;
-                break;
-            case 3:
-                resultado = num1 * num2;
-                break;
-            case 4:
-                resultado = num1 / num2;
-                break;
-            case 5:
-                resultado = num1 % num2;
-                break;
-            case 6:
-                if (num2 <= num1)
-                {
-                    Console.WriteLine("El numero 2 debe de ser mayor al numero 1");
-                }
-                else
-                {
-                    for (decimal i = num1; i <= num2; i++)
-                    {
-                        resultado = resultado + i;
-                    }
-                }
-                break;
-            default:
-                resultado = 0;
-                Console.WriteLine("No se realizo ninguna operacion");
-                break;
-        }
+           
+         //Prompt for user input
+         Console.WriteLine("Please, enter the first number");
+         num1 = Convert.ToDecimal(Console.ReadLine());
 
-        Console.WriteLine($"El resultado de la operacion seleccionada es: {resultado}");
+         Console.WriteLine("Please, enter the second number");
+         num2 = Convert.ToDecimal(Console.ReadLine());
+         
+
+
+         decimal resultado = 0;
+
+         //Decide which operation is needed based on selected option
+
+            switch (resp)
+            {
+                case 1:
+                    resultado = num1 + num2;
+                    break;
+                case 2:
+                    resultado = num1 - num2;
+                    break;
+                case 3:
+                    resultado = num1 * num2;
+                    break;
+                case 4:
+                    resultado = num1 / num2;
+                    break;
+                case 5:
+                    resultado = num1 % num2;
+                    break;
+                case 6:
+                    if (num2 <= num1)
+                    {
+                        Console.WriteLine("El numero 2 debe de ser mayor al numero 1");
+                    }
+                    else
+                    {
+                        for (decimal i = num1; i <= num2; i++)
+                        {
+                            resultado = resultado + i;
+                        }
+                    }
+                    break;
+                default:
+                    throw new Exception("Se selecciono una opcion del menu invalida.");
+            }
+
+            Console.WriteLine($"El resultado de la operacion seleccionada es: {resultado}");
+        }
     }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("No es posible dividir entre Cero");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Presiona cualquier teclapara continuar.");
+        Console.ReadLine();
+        Console.Clear();
+    }
+   
 } while (resp >= 0);
 
 Console.WriteLine("********** - Thank you for using the sample calculator app - **********");
